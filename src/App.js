e6392be6779b4fede4f665101e4d33436c5a2d11
JC-Ge <gxl200896@163.com>
2021-06-15 22:27:31 +0800

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World jc-ge
+          Hello World
         </a>
       </header>
     </div>
